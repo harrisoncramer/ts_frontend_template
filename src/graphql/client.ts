ec641ecaea45875e8b60e4f57483179d69ed0dad
Harrison Cramer <harrisoncramer@gmail.com>
2020-08-21 18:25:03 -0400

    fixed REACT_APP_API variable in httpLink for apollo client

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
-const httpLink = createHttpLink({ uri: "https://crwn-clothing.com" });
+const httpLink = createHttpLink({ uri: process.env.REACT_APP_API as string });
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
