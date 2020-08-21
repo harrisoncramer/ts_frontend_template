a6eec13f32cb78010d5db35fcf6a240111a7175f
Harrison Cramer <harrisoncramer@gmail.com>
2020-08-21 18:24:02 -0400

    simplified apollo client

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,32 +1,11 @@
-/* istanbul ignore file */
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  ApolloLink,
-} from "@apollo/client";
-
-import { onError } from "@apollo/client/link/error";
+const httpLink = createHttpLink({ uri: "https://crwn-clothing.com" });
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: process.env.REACT_APP_API as string,
-    }),
-  ]),
-  cache: new InMemoryCache({
-    addTypename: false,
-  }),
+  link: httpLink,
+  cache,
   connectToDevTools: true,
 });
 
